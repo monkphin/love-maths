5c920458ce65a85d00d31a6e69c956311d420920
Darren Burrows <darren.burrows@gmail.com>
2024-06-20 23:16:45 +0000

    added code for division test

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,6 +17,12 @@ document.addEventListener("DOMContentLoaded", function() {
     })
   }
 
+  document.getElementById("answer-box").addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        checkAnswer();
+    }
+  })
+
   runGame("addition");
 
 })
@@ -27,6 +33,10 @@ document.addEventListener("DOMContentLoaded", function() {
  */
 
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
     //Creates a random number between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
@@ -36,6 +46,8 @@ function runGame(gameType) {
     } else if (gameType === "multiply") {
         displayMultiplicationQuestion(num1, num2) 
     } else if (gameType === "subtract") {
+        displaySubtractionQuestion(num1, num2) 
+    } else if (gameType === "division") {
         displaySubtractionQuestion(num1, num2) 
     } else {
         alert(`unknown game type ${gameType}`)
@@ -82,6 +94,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if(operator === "-") {
         return [operand1 - operand2, "subtract"];
+    } else if(operator === "/") {
+        return [operand1 / operand2, "division"];
     } else {
         alert (`Unimplemented Operator ${operator}`);
         throw (`Unimplemented Operator ${operator}. Aborting`);
@@ -125,7 +139,8 @@ function displaySubtractionQuestion(operand1, operand2) {
 }
 
 function displayDivisionQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
+    operand1 = operand1 * operand2;
+    document.getElementById('operand1').textContent = operand1; // Checks if operand1 is bigger than Operand2 and if so, returns Operand2, if not returns operand1. 
+    document.getElementById('operand1').textContent = operand2;
     document.getElementById('operator').textContent = "/";
 }
