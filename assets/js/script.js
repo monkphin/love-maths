027feedd9e1429ba5af24e5a3002c750d50026fa
Darren Burrows <darren.burrows@gmail.com>
2024-06-20 22:32:28 +0000

    added code to increment score and incorrect answer tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,8 +40,8 @@ function runGame(gameType) {
 }
 
 /**
- * Checks the answer agains the first element 
- * in the returned CalculatedCorrectANswer Array
+ * Checks the answer against the first element 
+ * in the returned CalculatedCorrectAnswer Array
  */
 
 function checkAnswer() {
@@ -51,8 +51,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got this right :D");
+        incrementScore();
     } else {
         alert (`Awww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -78,12 +80,23 @@ function calculateCorrectAnswer() {
 
 }
 
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
+
 function incrementScore() {
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 
 }
 
-function incrementWrongAnswer() {
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
 
+function incrementWrongAnswer() {
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
